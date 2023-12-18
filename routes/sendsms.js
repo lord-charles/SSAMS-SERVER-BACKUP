35a5d4303d5c6cae7c34de116f7fd19a1211210e
lord-charles <mwanikicharles226@gmail.com>
2023-12-18 19:29:05 +0300

    43

--- routes/sendsms.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { sendsms, sendsms2, sendsms3 } = require("../controller/sendsms");
-
-router.post("/", sendsms);
-router.post("/advata", sendsms2);
-router.post("/textsms", sendsms3);
-
-module.exports = router;
